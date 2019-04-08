0762342919f985e7a054eb95c473aa21d721a6d1
Yoann RIBEIRO <yoribeir@student.42.fr>
2019-04-08 14:47:50 +0200

    clean header

--- includes/filler.h
+++ includes/filler.h
@@ -6,7 +6,7 @@
 /*   By: yoribeir <yoribeir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 15:56:11 by yoribeir          #+#    #+#             */
-/*   Updated: 2019/04/08 14:42:26 by yoribeir         ###   ########.fr       */
+/*   Updated: 2019/04/08 14:47:35 by yoribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ typedef	struct	s_parser
 }				t_parser;
 
 /*
-** PARSER
+** -------- PARSER ----------
 */
 
 int				get_player(t_parser *p);
@@ -47,7 +47,7 @@ int				get_piece(t_parser *p);
 int				parser(t_parser *p);
 
 /*
-** PLAYER
+** -------- PLAYER ----------
 */
 
 int				get_position(t_parser *p);
@@ -55,14 +55,14 @@ int				is_placeable(t_parser *p, int boardy, int boardx);
 int				solve(t_parser *p);
 
 /*
-** PIECE
+** -------- PIECE ----------
 */
 
 int				trim_piece(t_parser *p, char **ogpiece);
 void			get_shape(t_parser *p, char **ogpiece);
 
 /*
-** HEATMAP
+** -------- HEATMAP ----------
 */
 
 int				get_heatmap(t_parser *p);
@@ -70,17 +70,12 @@ void			print_hmap(t_parser *p);
 void			fill_hmap(t_parser *p);
 
 /*
-** UTILS
+** -------- UTILS ----------
 */
 
 void			init_struct(t_parser *p);
-void			print_board(t_parser *p);
-void			print_piece(t_parser *p, char **piece);
-void			print_parsing(t_parser *p);
-void			free_piece(t_parser *p);
 void			free_2darray(char **arr, int size);
 void			free_hmap(t_parser *p);
-void			ft_intdel(int **as);
 int				ft_puterror(char *s);
 void			print_result(t_parser *p);
 
